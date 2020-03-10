2dd0bc0635f99ea5fceac5104e1eb0439a3e11f1
Luke <ReventonC@users.noreply.github.com>
2020-03-10 07:22:59 -0400

    add

---
+++ js/main.js
@@ -0,0 +1,81 @@
+
+
+
+$(document).ready(() => {
+    $(".task-tab").click((e) => {
+        $(".task-tab").removeClass("active", 100)
+        $(e.target).closest(".task-tab").addClass("active", 100)
+    })
+
+    $(".task-progress").click((e)=>{
+        $(".task-page").addClass("n");
+        $(".page-1").removeClass("n")
+        console.log("hi")
+    })
+    
+    $(".task-profile").click((e)=>{
+        $(".task-page").addClass("n");
+        $(".page-2").removeClass("n")
+    })
+
+    $(".a-window").mousedown((e) => {
+        $(".a-window").css("z-index", "10")
+        $(e.target).closest(".a-window").css("z-index", "999")
+    })
+    $(".dot1").click((e) => {
+        $(e.target).closest(".window").css("opacity", "0")
+        console.log("why")
+    })
+})
+
+
+// target elements with the "draggable" class
+interact('.draggable')
+    .draggable({
+        // enable inertial throwing
+        inertia: false,
+        // keep the element within the area of it's parent
+        modifiers: [
+            interact.modifiers.restrictRect({
+                restriction: 'parent',
+                endOnly: true
+            })
+        ],
+        // enable autoScroll
+        autoScroll: true,
+
+        listeners: {
+            // call this function on every dragmove event
+            move: dragMoveListener,
+
+            // call this function on every dragend event
+            end(event) {
+                //var textEl = event.target.querySelector('p')
+
+                // textEl && (textEl.textContent =
+                //     'moved a distance of ' +
+                //     (Math.sqrt(Math.pow(event.pageX - event.x0, 2) +
+                //         Math.pow(event.pageY - event.y0, 2) | 0))
+                //         .toFixed(2) + 'px')
+            }
+        }
+    })
+
+function dragMoveListener(event) {
+    var target = event.target
+    // keep the dragged position in the data-x/data-y attributes
+    var x = (parseFloat(target.getAttribute('data-x')) || 0) + event.dx
+    var y = (parseFloat(target.getAttribute('data-y')) || 0) + event.dy
+
+    // translate the element
+    target.style.webkitTransform =
+        target.style.transform =
+        'translate(' + x + 'px, ' + y + 'px)'
+
+    // update the posiion attributes
+    target.setAttribute('data-x', x)
+    target.setAttribute('data-y', y)
+}
+
+// this function is used later in the resizing and gesture demos
+window.dragMoveListener = dragMoveListener
